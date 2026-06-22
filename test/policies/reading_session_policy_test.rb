cc06a874650386a97cbec5b40203d1ba8b231a96
Ellie Kweon <kweondew@gmail.com>
2026-06-22 15:43:13 -0700

    Fix ReadingSessionPolicy scope and show? to handle admin access, add missing policy tests

--- test/policies/reading_session_policy_test.rb
+++ test/policies/reading_session_policy_test.rb
@@ -1,5 +1,28 @@
 require "test_helper"
 class ReadingSessionPolicyTest < ActiveSupport::TestCase
+  test "logged-in user can index reading sessions" do
+    assert ReadingSessionPolicy.new(users(:leika), ReadingSession).index?
+  end
+
+  test "admin can index reading sessions" do
+    assert ReadingSessionPolicy.new(users(:admin), ReadingSession).index?
+  end
+
+  test "owner can show their own session" do
+    assert ReadingSessionPolicy.new(users(:leika), reading_sessions(:one)).show?
+  end
+
+  test "non-owner cannot show someone else's session" do
+    refute ReadingSessionPolicy.new(users(:jaina), reading_sessions(:one)).show?
+  end
+
+  test "admin can show any session" do
+    assert ReadingSessionPolicy.new(users(:admin), reading_sessions(:one)).show?
+  end
+
+  test "logged-in user can create a reading session" do
+    assert ReadingSessionPolicy.new(users(:leika), ReadingSession.new).create?
+  end
   test "owner can update their own session" do
     assert ReadingSessionPolicy.new(users(:leika), reading_sessions(:one)).update?
   end
