b0b625432b6a6c2ff0b368b30224671d936b2e94
Jade Ornelas <jade@ornelas.io>
2026-06-24 23:28:39 -0700

    refactor: authorize dashboard log actions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    authorize UserBook
+    @user_books_by_status = policy_scope(UserBook).includes(:book).group_by(&:status)
   end
 end
