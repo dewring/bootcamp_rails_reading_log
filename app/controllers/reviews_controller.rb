b812f4e6a3aaaa07b66ca9787f6a1ed287a60a2a
Jade Ornelas <jade@ornelas.io>
2026-06-24 23:29:00 -0700

    test: enforce review policy access

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,20 +4,25 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [ :edit, :update ]
 
   def new
-    if current_user.reviews.exists?(book: @book)
-      redirect_to book_path(@book), alert: "You can review only once per book "
-      return
-    end
-    @review = Review.new
-  end
-  def create
-    if current_user.reviews.exists?(book: @book)
+    @review = @book.reviews.build(user: current_user)
+    unless policy(@review).new?
       redirect_to book_path(@book), alert: "You can review only once per book "
       return
     end
 
+    authorize @review
+  end
+
+  def create
     @review = @book.reviews.build(review_params)
     @review.user = current_user
+
+    unless policy(@review).create?
+      redirect_to book_path(@book), alert: "You can review only once per book "
+      return
+    end
+
+    authorize @review
 
     if @review.save
       redirect_to book_path(@book), notice: "Review added!"
@@ -25,9 +30,13 @@ class ReviewsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def edit
+    authorize @review
   end
+
   def update
+    authorize @review
     if @review.update(review_params)
       redirect_to book_path(@book), notice: "Review updated!"
     else
