66249637e3e4ef02a1a748fb4ccbe2898f85c864
Jade Ornelas <yknx.4.b@gmail.com>
2026-07-03 14:05:40 -0700

    fix eager loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
     books = policy_scope(Book)
-    book_most_read = books.most_read.with_attached_cover_image
+    book_most_read = books.most_read.includes([ :cover_image_attachment ])
     @most_read = if params[:genre].present?
       book_most_read.joins(:genres).where(genres: { name: params[:genre] }).limit(8)  # genre 있을 때
     else
       book_most_read.limit(8)  # genre 없을 때
     end
-    @new_books = books.with_attached_cover_image.order(created_at: :desc).limit(8)
+    @new_books = books.includes([ :cover_image_attachment ]).order(created_at: :desc).limit(8)
   end
 end
