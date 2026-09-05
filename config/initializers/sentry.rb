e39cdb21bac7eabda10839eb2bca695bd3ad1c2c
dewring <kweondew@gmail.com>
2026-09-05 16:01:27 -0700

    changed env basis on production env

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV["GLITCHTIP_DSN"]
+  config.dsn = ENV["SENTRY_DSN"]
   config.traces_sample_rate = 1.0
   config.enabled_environments = %w[production development]
   config.release = ENV["SOURCE_COMMIT"]
