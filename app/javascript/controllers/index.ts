f89d2f4a608ccc7425d3e2e86591c62f5901ae81
dewring <kweondew@gmail.com>
2026-09-14 19:56:11 -0700

    Fix CSP-blocked status dropdown with a Stimulus controller

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -6,6 +6,7 @@ import QuickAddController from "./quick_add_controller"
 import IsbnScannerController from "./isbn_scanner_controller"
 import NavbarController from "./navbar_controller"
 import BookPickerController from "./book_picker_controller"
+import SubmitOnChangeController from "./submit_on_change_controller"
 
 application.register("search", SearchController)
 application.register("progress-bar", ProgressBarController)
@@ -13,3 +14,4 @@ application.register("quick-add", QuickAddController)
 application.register("isbn-scanner", IsbnScannerController)
 application.register("navbar", NavbarController)
 application.register("book-picker", BookPickerController)
+application.register("submit-on-change", SubmitOnChangeController)
