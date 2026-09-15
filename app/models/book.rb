ce3dcbaf41cb35e94a543458cc5494b73b325a15
dewring <kweondew@gmail.com>
2026-09-14 19:55:35 -0700

    Add Book#book_clubs inverse association

--- app/models/book.rb
+++ app/models/book.rb
@@ -21,6 +21,7 @@ class Book < ApplicationRecord
   has_many :genres, through: :book_genres
   has_many :reviews, dependent: :destroy
   has_many :book_editions, dependent: :destroy
+  has_many :book_clubs, foreign_key: :current_book_id
 
   # 5. Scopes
   scope :most_read, -> {
